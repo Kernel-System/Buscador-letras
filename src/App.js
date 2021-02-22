bb8090054b60ed46e3ca00e4c3a96d6d9d9aa63e
Edson David Puente Guerrero <edpg_@hotmail.com>
2021-02-22 11:46:19 -0700

    Proyecto buscador

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import Formulario from "./components/Formulario";
+import Cancion from "./components/Cancion";
+import Info from "./components/Info";
+import axios from "axios";
 
 function App() {
+  const [busLetra, setBusLetra] = useState({});
+
+  const [letra, setLetra] = useState("");
+  const [info, setInfo] = useState({});
+
+  useEffect(() => {
+    if (Object.keys(busLetra).length === 0) return;
+
+    const consultarApiLetra = async () => {
+      const url_letra = `https://api.lyrics.ovh/v1/${busLetra.artista}/${busLetra.cancion}`;
+      const url_info = `https://www.theaudiodb.com/api/v1/json/1/search.php?s=${busLetra.artista}`;
+
+      const [letraRes, infoRes] = await Promise.all([
+        axios.get(url_letra),
+        axios.get(url_info),
+      ]);
+      setLetra(letraRes.data.lyrics);
+      setInfo(infoRes.data.artists[0]);
+
+      setBusLetra({});
+    };
+
+    consultarApiLetra();
+  }, [busLetra, info]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Formulario setBusLetra={setBusLetra} />
+      <div className="container mt-5">
+        <div className="row">
+          <div className="col-md-6">
+            <Info info={info} />
+          </div>
+          <div className="col-md-6">
+            <Cancion letra={letra} />
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
